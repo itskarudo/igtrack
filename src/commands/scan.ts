a014cf6fbb3e02c2ff48eeb0f3bcfddb95d50670
itskarudo <itskarudo@protonmail.com>
2023-03-16 20:51:44 +0000

    FUCK FUCKING INSTAGRAM
    FUCK INSTAGRAM AND THEIR CONSTANT THIRST FOR CHANGING CLASS NAMES FUCK
    FUCK FUCK FUCK FUCK THEM

--- src/commands/scan.ts
+++ src/commands/scan.ts
@@ -74,7 +74,8 @@ export default class Scan extends Command {
     }
 
     await page.goto(`https://instagram.com/${username}/followers`);
-    await page.waitForSelector("._ac2a");
+    await page.waitForSelector("._aba8");
+    await page.waitForSelector("._aba8", {hidden: true});
 
     const newFollowers = await page.evaluate(() => {
       return new Promise<string[]>((resolve, _) => {
@@ -83,7 +84,7 @@ export default class Scan extends Command {
         const int = setInterval(() => {
           followers = Array.from(
             document.getElementsByClassName(
-              "_ab8w  _ab94 _ab97 _ab9f _ab9k _ab9p  _ab9- _aba8 _abcm"
+              "x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz xh8yej3 x193iq5w x1lliihq x1dm5mii x16mil14 xiojian x1yutycm"
             )
           );
           followers.at(-1)?.scrollIntoView();
@@ -94,7 +95,7 @@ export default class Scan extends Command {
               followers.map(
                 (f) =>
                   f.getElementsByClassName(
-                    "_ab8y  _ab94 _ab97 _ab9f _ab9k _ab9p _abcm"
+                    "x9f619 xjbqb8w x1rg5ohu x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1"
                   )[0].innerHTML
               )
             );
